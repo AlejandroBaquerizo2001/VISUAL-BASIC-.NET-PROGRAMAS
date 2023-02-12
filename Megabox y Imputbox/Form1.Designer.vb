01a37f8165457ca73f559dcdf947cf671b0d1b1d
Alejandro Baquerizo <89667907+AlejandroBaquerizo2001@users.noreply.github.com>
2023-02-11 20:59:48 -0500

    Add files via upload

---
+++ Megabox y Imputbox/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Btningresar = New System.Windows.Forms.Button()
+        Me.Btnsalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btningresar
+        '
+        Me.Btningresar.Location = New System.Drawing.Point(53, 49)
+        Me.Btningresar.Name = "Btningresar"
+        Me.Btningresar.Size = New System.Drawing.Size(195, 47)
+        Me.Btningresar.TabIndex = 0
+        Me.Btningresar.Text = "Ingrese Datos"
+        Me.Btningresar.UseVisualStyleBackColor = True
+        '
+        'Btnsalir
+        '
+        Me.Btnsalir.Location = New System.Drawing.Point(352, 132)
+        Me.Btnsalir.Name = "Btnsalir"
+        Me.Btnsalir.Size = New System.Drawing.Size(112, 34)
+        Me.Btnsalir.TabIndex = 1
+        Me.Btnsalir.Text = "Salir"
+        Me.Btnsalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(496, 212)
+        Me.Controls.Add(Me.Btnsalir)
+        Me.Controls.Add(Me.Btningresar)
+        Me.Name = "Form1"
+        Me.Text = "Megabox y Inputbox"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Btningresar As Button
+    Friend WithEvents Btnsalir As Button
+End Class
---
+++ Megabox y Imputbox/Form1.vb
@@ -0,0 +1,28 @@
+﻿'Problemática:
+'Crear un programa que permita ingresar el nombre de un usuario en un Inputbox y después 
+'mostrar el saludo respectivo en un MessageBox.
+'Además mostrar un botón de salir que pregunte y seleccione la opción de salir antes 
+'de cerrar la aplicación
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Btningresar_Click(sender As Object, e As EventArgs) Handles Btningresar.Click
+        'Declaracion de variables
+        Dim nombre As String
+        'Entrada de datos
+        nombre = InputBox("Ingrese Nombre", "Registro de Datos", "Nombre", 100, 0)
+        MessageBox.Show("Bienvenido Usuario: " & nombre, "Registro de Datos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Btnsalir_Click(sender As Object, e As EventArgs) Handles Btnsalir.Click
+        Dim opcion As DialogResult
+        opcion = MessageBox.Show("Realmente desea salir de la APP?", "Salir de la App", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If (opcion = DialogResult.Yes) Then
+            Me.Close()
+
+        End If
+    End Sub
+End Class
