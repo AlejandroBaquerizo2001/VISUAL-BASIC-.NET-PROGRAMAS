a0d4d86fab247632b30a66f33db2c7ef8763bd2f
Alejandro Baquerizo <89667907+AlejandroBaquerizo2001@users.noreply.github.com>
2023-02-11 20:50:16 -0500

    Add files via upload

---
+++ Control_Timer/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Btnplay = New System.Windows.Forms.Button()
+        Me.Btnstop = New System.Windows.Forms.Button()
+        Me.lblfecha = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Black", 28.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(12, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(288, 74)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IcanatolT"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Black", 28.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(12, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(288, 74)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "IcanatolT"
+        '
+        'Btnplay
+        '
+        Me.Btnplay.Location = New System.Drawing.Point(379, 24)
+        Me.Btnplay.Name = "Btnplay"
+        Me.Btnplay.Size = New System.Drawing.Size(112, 34)
+        Me.Btnplay.TabIndex = 2
+        Me.Btnplay.Text = "Play"
+        Me.Btnplay.UseVisualStyleBackColor = True
+        '
+        'Btnstop
+        '
+        Me.Btnstop.Location = New System.Drawing.Point(379, 110)
+        Me.Btnstop.Name = "Btnstop"
+        Me.Btnstop.Size = New System.Drawing.Size(112, 34)
+        Me.Btnstop.TabIndex = 3
+        Me.Btnstop.Text = "Stop"
+        Me.Btnstop.UseVisualStyleBackColor = True
+        '
+        'lblfecha
+        '
+        Me.lblfecha.AutoSize = True
+        Me.lblfecha.Location = New System.Drawing.Point(30, 9)
+        Me.lblfecha.Name = "lblfecha"
+        Me.lblfecha.Size = New System.Drawing.Size(63, 25)
+        Me.lblfecha.TabIndex = 4
+        Me.lblfecha.Text = "Label3"
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(533, 186)
+        Me.Controls.Add(Me.lblfecha)
+        Me.Controls.Add(Me.Btnstop)
+        Me.Controls.Add(Me.Btnplay)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Control Timer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Btnplay As Button
+    Friend WithEvents Btnstop As Button
+    Friend WithEvents lblfecha As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ Control_Timer/Form1.vb
@@ -0,0 +1,33 @@
+﻿'Practica Control Timer:
+'Implementar una aplicación VB que permita mostrar la hora actualizada al Segundo,
+'y que permita crear una animación simple entre dos etiquetas de texto
+Public Class Form1
+    Private num As Integer = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (num Mod 2 = 0) Then
+            Label1.Visible = True
+            Label2.Visible = False
+        Else
+            Label1.Visible = False
+            Label2.Visible = True
+        End If
+        num = num + 1
+    End Sub
+
+    Private Sub Btnplay_Click(sender As Object, e As EventArgs) Handles Btnplay.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Btnstop_Click(sender As Object, e As EventArgs) Handles Btnstop.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        lblfecha.Text = TimeOfDay
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblfecha.Text = TimeOfDay
+    End Sub
+End Class
