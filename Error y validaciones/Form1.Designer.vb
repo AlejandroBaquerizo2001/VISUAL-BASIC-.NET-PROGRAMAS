9e702206a0204b5b539c1173e1fc783687766216
Alejandro Baquerizo <89667907+AlejandroBaquerizo2001@users.noreply.github.com>
2023-02-11 20:58:18 -0500

    Add files via upload

---
+++ Error y validaciones/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblSalir = New System.Windows.Forms.LinkLabel()
+        Me.BtnEnviar = New System.Windows.Forms.Button()
+        Me.BtnNuevo = New System.Windows.Forms.Button()
+        Me.TextDescripcion = New System.Windows.Forms.TextBox()
+        Me.TextSalario = New System.Windows.Forms.TextBox()
+        Me.TextEdad = New System.Windows.Forms.TextBox()
+        Me.TextNombre = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.erroricono = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.erroricono, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblSalir)
+        Me.GroupBox1.Controls.Add(Me.BtnEnviar)
+        Me.GroupBox1.Controls.Add(Me.BtnNuevo)
+        Me.GroupBox1.Controls.Add(Me.TextDescripcion)
+        Me.GroupBox1.Controls.Add(Me.TextSalario)
+        Me.GroupBox1.Controls.Add(Me.TextEdad)
+        Me.GroupBox1.Controls.Add(Me.TextNombre)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(754, 426)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese Datos"
+        '
+        'lblSalir
+        '
+        Me.lblSalir.AutoSize = True
+        Me.lblSalir.Location = New System.Drawing.Point(634, 27)
+        Me.lblSalir.Name = "lblSalir"
+        Me.lblSalir.Size = New System.Drawing.Size(45, 25)
+        Me.lblSalir.TabIndex = 10
+        Me.lblSalir.TabStop = True
+        Me.lblSalir.Text = "Salir"
+        '
+        'BtnEnviar
+        '
+        Me.BtnEnviar.Location = New System.Drawing.Point(596, 361)
+        Me.BtnEnviar.Name = "BtnEnviar"
+        Me.BtnEnviar.Size = New System.Drawing.Size(112, 34)
+        Me.BtnEnviar.TabIndex = 9
+        Me.BtnEnviar.Text = "Enviar"
+        Me.BtnEnviar.UseVisualStyleBackColor = True
+        '
+        'BtnNuevo
+        '
+        Me.BtnNuevo.Location = New System.Drawing.Point(596, 279)
+        Me.BtnNuevo.Name = "BtnNuevo"
+        Me.BtnNuevo.Size = New System.Drawing.Size(112, 34)
+        Me.BtnNuevo.TabIndex = 8
+        Me.BtnNuevo.Text = "Nuevo"
+        Me.BtnNuevo.UseVisualStyleBackColor = True
+        '
+        'TextDescripcion
+        '
+        Me.TextDescripcion.Location = New System.Drawing.Point(141, 288)
+        Me.TextDescripcion.Multiline = True
+        Me.TextDescripcion.Name = "TextDescripcion"
+        Me.TextDescripcion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextDescripcion.Size = New System.Drawing.Size(396, 122)
+        Me.TextDescripcion.TabIndex = 7
+        '
+        'TextSalario
+        '
+        Me.TextSalario.Location = New System.Drawing.Point(141, 207)
+        Me.TextSalario.Name = "TextSalario"
+        Me.TextSalario.Size = New System.Drawing.Size(199, 31)
+        Me.TextSalario.TabIndex = 6
+        '
+        'TextEdad
+        '
+        Me.TextEdad.Location = New System.Drawing.Point(141, 126)
+        Me.TextEdad.Name = "TextEdad"
+        Me.TextEdad.Size = New System.Drawing.Size(199, 31)
+        Me.TextEdad.TabIndex = 5
+        '
+        'TextNombre
+        '
+        Me.TextNombre.Location = New System.Drawing.Point(141, 49)
+        Me.TextNombre.Name = "TextNombre"
+        Me.TextNombre.Size = New System.Drawing.Size(396, 31)
+        Me.TextNombre.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 291)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(104, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Descripción"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 213)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Salario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 132)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Edad"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombres"
+        '
+        'erroricono
+        '
+        Me.erroricono.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(779, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Error provider y Validacion"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.erroricono, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblSalir As LinkLabel
+    Friend WithEvents BtnEnviar As Button
+    Friend WithEvents BtnNuevo As Button
+    Friend WithEvents TextDescripcion As TextBox
+    Friend WithEvents TextSalario As TextBox
+    Friend WithEvents TextEdad As TextBox
+    Friend WithEvents TextNombre As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents erroricono As ErrorProvider
+End Class
---
+++ Error y validaciones/Form1.vb
@@ -0,0 +1,58 @@
+﻿'Problematica de ERROR PROVIDER Y VALIDACIONES
+'Crear un programa que permita ingresar de manera obligatoria los nombres, la edad y el salario de una persona;
+'además todos los campos de texto deben de tener un mensaje emergente que ayude al usuario final con el contenido
+'que debe ingresar.
+Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnEnviar_Click(sender As Object, e As EventArgs) Handles BtnEnviar.Click
+        Try
+            If Me.ValidateChildren And TextNombre.Text <> String.Empty And Val(TextEdad.Text) - Int(Val(TextEdad.Text)) = 0 And TextSalario.Text <> String.Empty Then
+                MessageBox.Show("Datos registrados correctamente", "Registro de Usuarios", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Ingrese correctamente algunos datos remarcados", "Registro de Usuarios", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextNombre_TextChanged(sender As Object, e As EventArgs) Handles TextNombre.TextChanged
+
+    End Sub
+
+    Private Sub TextNombre_Validating(sender As Object, e As CancelEventArgs) Handles TextNombre.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.erroricono.SetError(sender, "")
+        Else
+            Me.erroricono.SetError(sender, "Ingrese el salario este dato es obligatorio")
+        End If
+    End Sub
+
+    Private Sub TextEdad_TextChanged(sender As Object, e As EventArgs) Handles TextEdad.TextChanged
+
+    End Sub
+
+    Private Sub TextEdad_Validating(sender As Object, e As CancelEventArgs) Handles TextEdad.Validating
+        If (Val(TextEdad.Text) - Int(Val(TextEdad.Text)) = 0) Then
+            Me.erroricono.SetError(sender, "")
+        Else
+            Me.erroricono.SetError(sender, "Ingrese una edad valida, este dato es obligatorio")
+        End If
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles BtnNuevo.Click
+        TextEdad.Clear()
+        TextSalario.Clear()
+        TextNombre.Clear()
+        TextDescripcion.Clear()
+    End Sub
+
+    Private Sub lblSalir_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblSalir.LinkClicked
+        Me.Close()
+    End Sub
+End Class
