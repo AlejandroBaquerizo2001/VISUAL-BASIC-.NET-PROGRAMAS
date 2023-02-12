084e4b28abd885fd936c852b266ee5bad813c804
Alejandro Baquerizo <89667907+AlejandroBaquerizo2001@users.noreply.github.com>
2023-02-11 20:59:08 -0500

    Add files via upload

---
+++ Estructura_Case/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.BtnCalcular = New System.Windows.Forms.Button()
+        Me.BtnNuevo = New System.Windows.Forms.Button()
+        Me.Textnumero = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Textdia = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BtnSalir)
+        Me.GroupBox1.Controls.Add(Me.BtnCalcular)
+        Me.GroupBox1.Controls.Add(Me.BtnNuevo)
+        Me.GroupBox1.Controls.Add(Me.Textnumero)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(556, 181)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese Datos"
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Location = New System.Drawing.Point(421, 118)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(112, 34)
+        Me.BtnSalir.TabIndex = 4
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = True
+        '
+        'BtnCalcular
+        '
+        Me.BtnCalcular.Location = New System.Drawing.Point(233, 118)
+        Me.BtnCalcular.Name = "BtnCalcular"
+        Me.BtnCalcular.Size = New System.Drawing.Size(120, 34)
+        Me.BtnCalcular.TabIndex = 3
+        Me.BtnCalcular.Text = "Calcular"
+        Me.BtnCalcular.UseVisualStyleBackColor = True
+        '
+        'BtnNuevo
+        '
+        Me.BtnNuevo.Location = New System.Drawing.Point(56, 118)
+        Me.BtnNuevo.Name = "BtnNuevo"
+        Me.BtnNuevo.Size = New System.Drawing.Size(112, 34)
+        Me.BtnNuevo.TabIndex = 2
+        Me.BtnNuevo.Text = "Nuevo"
+        Me.BtnNuevo.UseVisualStyleBackColor = True
+        '
+        'Textnumero
+        '
+        Me.Textnumero.Location = New System.Drawing.Point(151, 46)
+        Me.Textnumero.Name = "Textnumero"
+        Me.Textnumero.Size = New System.Drawing.Size(202, 31)
+        Me.Textnumero.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Número"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Textdia)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(25, 209)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(556, 118)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Día de la Semana"
+        '
+        'Textdia
+        '
+        Me.Textdia.Location = New System.Drawing.Point(151, 55)
+        Me.Textdia.Name = "Textdia"
+        Me.Textdia.Size = New System.Drawing.Size(382, 31)
+        Me.Textdia.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(56, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Día"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(611, 342)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Estructura Selectiva"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BtnSalir As Button
+    Friend WithEvents BtnCalcular As Button
+    Friend WithEvents BtnNuevo As Button
+    Friend WithEvents Textnumero As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Textdia As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Estructura_Case/Form1.vb
@@ -0,0 +1,45 @@
+﻿'Problema: Ingresar un número por teclado y determinar su representación en un día
+'de la semana.
+'Ej: Si se ingresa 3 debera mostrar miércoles, si ingresa 7 mostrará domingo; pero si no 
+'ingresa un número fuera del rango entre 1 y 7 mostrar el mensaje"Error Intente con un nuevo
+''nuevo numero"
+Public Class Form1
+    Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles BtnCalcular.Click
+        'Declaración de variables
+        Dim num As Integer
+        Dim dia As String
+        'Entrada de datos
+        num = Val(Textnumero.Text)
+        'Proceso
+        Select Case (num)
+            Case 1
+                dia = "Lunes"
+            Case 2
+                dia = "Martes"
+            Case 3
+                dia = "Miercoles"
+            Case 4
+                dia = "Jueves"
+            Case 5
+                dia = "Viernes"
+            Case 6
+                dia = "Sábado"
+            Case 7
+                dia = "Domingo"
+            Case Else
+                dia = "Error intente con un nuevo número"
+        End Select
+        'Salida de información
+        Textdia.Text = dia
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles BtnNuevo.Click
+        Textdia.Clear()
+        Textnumero.Clear()
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        Me.Close()
+    End Sub
+
+End Class
